d7a76cae1a01ee8b1f9cfd77a28fa30e372a31de
ElBachir Toure <bassirou.toure@esp.sn>
2024-05-12 18:07:37 +0000

    starter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <p className="text-bold">Hello World!</p>
+      <p className="text-bold">My Next Started App!</p>
     </div>
   );
 }
